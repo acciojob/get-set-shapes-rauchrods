742476f34fdc737298ec417b3d62af25222ef505
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-04-11 18:46:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    get get_width() {
+        return this.width;
+    }
 
-class Square extends Animal {}
+    get get_height() {
+        return this.height;
+    }
+
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+class Square extends Rectangle {
+	    constructor(width, height) {
+        super(width, height);
+    }
+
+    getPerimeter() {
+        return this.width*2 + this.height*2;
+    }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
